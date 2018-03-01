1c7f2e5f491f8d917af4aad47e4edfd76883546b
falafel-v-postel <zztopgrodno@gmail.com>
2018-03-01 18:11:09 +0300

    zerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var x = 1;
+  var zeros =0;
+  while (Math.pow(5, x) <=number ){
+    zeros = zeros + parseInt(number/Math.pow(5, x));
+    x++;
+  }
+    return zeros;
 }
